5aa618ebb9981613bf7d2bb58671547987b084a6
Anton Lunenko <lunenko@dilegences.com>
2015-03-19 14:36:57 +0200

    continue add apostrophes

--- workbook/db/kohana_suntracker_queries.sql
+++ workbook/db/kohana_suntracker_queries.sql
@@ -50,13 +50,13 @@ INDEX `i_updated_at` (`updated_at`)
 
 CREATE TABLE IF NOT EXISTS `Gallery`
 (
-id INT(11) NOT NULL UNSIGNED AUTO_INCREMENT COMMENT 'Primary key',
-image_id INT(11) NULL DEFAULT NULL COMMENT 'Foreign key for table Image. Image id from Images',
-title VARCHAR(64) NOT NULL COMMENT 'Gallery name',
-alias VARCHAR(128) NOT NULL COMMENT 'Link at this page',
-status TINYINT(3) UNSIGNED NOT NULL DEFAULT '1' COMMENT 'Status. See Enum_Status',
-created_at TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
-updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
+`id` INT(11) NOT NULL UNSIGNED AUTO_INCREMENT COMMENT 'Primary key',
+`image_id` INT(11) NULL DEFAULT NULL COMMENT 'Foreign key for table Image. Image id from Images',
+`title` VARCHAR(64) NOT NULL COMMENT 'Gallery name',
+`alias` VARCHAR(128) NOT NULL COMMENT 'Link at this page',
+`status` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1' COMMENT 'Status. See Enum_Status',
+`created_at` TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
+`updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
 PRIMARY KEY `pk_id` (`id`),
 UNIQUE INDEX `ui_alias` (`alias`),
 INDEX `i_image_id` (`image_id`),
@@ -67,13 +67,13 @@ INDEX `i_updated_at` (`updated_at`)
 
 CREATE TABLE IF NOT EXISTS `Post`
 (
-id INT(11) NOT NULL UNSIGNED AUTO_INCREMENT COMMENT 'Primary key',
-image_id INT(11) NULL DEFAULT NULL COMMENT 'Foreign key for table Image. Image id from Images',
-content TEXT NOT NULL COMMENT 'Post content',
-alias VARCHAR(128) NOT NULL COMMENT 'Link at this page',
-status TINYINT(3) UNSIGNED NOT NULL DEFAULT '1' COMMENT 'Status. See Enum_Status',
-created_at TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
-updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
+`id` INT(11) NOT NULL UNSIGNED AUTO_INCREMENT COMMENT 'Primary key',
+`image_id` INT(11) NULL DEFAULT NULL COMMENT 'Foreign key for table Image. Image id from Images',
+`content` TEXT NOT NULL COMMENT 'Post content',
+`alias` VARCHAR(128) NOT NULL COMMENT 'Link at this page',
+`status` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1' COMMENT 'Status. See Enum_Status',
+`created_at` TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
+`updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
 PRIMARY KEY `pk_id` (`id`),
 UNIQUE INDEX `ui_alias` (`alias`),
 INDEX `i_image_id` (`image_id`),
@@ -84,15 +84,15 @@ INDEX `i_updated_at` (`updated_at`)
 
 CREATE TABLE IF NOT EXISTS `Product`
 (
-id INT(11) NOT NULL UNSIGNED AUTO_INCREMENT COMMENT 'Primary key',
-image_id INT(11) NULL DEFAULT NULL COMMENT 'Foreign key for table Image. Image id from Images',
-title VARCHAR(64) NOT NULL COMMENT 'Product name',
-description VARCHAR(128) NULL DEFAULT NULL COMMENT 'Small description after header',
-content TEXT NOT NULL COMMENT 'About product',
-alias VARCHAR(128) NOT NULL COMMENT 'Link at this page',
-status TINYINT(3) UNSIGNED NOT NULL DEFAULT '1' COMMENT 'Status. See Enum_Status',
-created_at TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
-updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
+`id` INT(11) NOT NULL UNSIGNED AUTO_INCREMENT COMMENT 'Primary key',
+`image_id` INT(11) NULL DEFAULT NULL COMMENT 'Foreign key for table Image. Image id from Images',
+`title` VARCHAR(64) NOT NULL COMMENT 'Product name',
+`description` VARCHAR(128) NULL DEFAULT NULL COMMENT 'Small description after header',
+`content` TEXT NOT NULL COMMENT 'About product',
+`alias` VARCHAR(128) NOT NULL COMMENT 'Link at this page',
+`status` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1' COMMENT 'Status. See Enum_Status',
+`created_at` TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
+`updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
 PRIMARY KEY `pk_id` (`id`),
 UNIQUE INDEX `ui_alias` (`alias`),
 INDEX `i_image_id` (`image_id`),
@@ -103,10 +103,10 @@ INDEX `i_updated_at` (`updated_at`)
 
 CREATE TABLE  IF NOT EXISTS `PageRelations`
 (
-id INT(11) NOT NULL UNSIGNED AUTO_INCREMENT COMMENT 'Primary key',
-page_id INT(11) NOT NULL COMMENT 'Page id',
-relation_id INT(11) NOT NULL COMMENT 'Relations id',
-type TINYINT(1) UNSIGNED NOT NULL COMMENT 'Type of page. See Enum_Relations',
+`id` INT(11) NOT NULL UNSIGNED AUTO_INCREMENT COMMENT 'Primary key',
+`page_id` INT(11) NOT NULL COMMENT 'Page id',
+`relation_id` INT(11) NOT NULL COMMENT 'Relations id',
+`type` TINYINT(1) UNSIGNED NOT NULL COMMENT 'Type of page. See Enum_Relations',
 PRIMARY KEY `pk_id` (`id`),
 UNIQUE INDEX `ui_page_relations` (`page_id`, `relation_id`, `type`)
 ) DEFAULT CHARSET='utf8' COLLATE='utf8_general_ci' ENGINE=InnoDB COMMENT 'Page relation';

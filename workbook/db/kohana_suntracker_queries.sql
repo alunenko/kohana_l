978c921c64ed348fbd3ec43b026aae5b50cef5f3
Anton Lunenko <lunenko@dilegences.com>
2015-03-19 14:03:04 +0200

    add fields: createdAt, updatedAt

--- workbook/db/kohana_suntracker_queries.sql
+++ workbook/db/kohana_suntracker_queries.sql
@@ -5,6 +5,8 @@ USE kohana;
 CREATE TABLE Image
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+createdAt timestamp NULL DEFAULT NULL COMMENT 'When was created',
+updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
 title VARCHAR(64) NOT NULL DEFAULT 'image title' COMMENT 'Image title',
 name VARCHAR(128) NOT NULL COMMENT 'Link at this page'
 ) CHARASET='utf8' COLLATE='utf8_general_ci' ENGINE=InnoDB COMMENT 'Single images';
@@ -12,6 +14,8 @@ name VARCHAR(128) NOT NULL COMMENT 'Link at this page'
 CREATE TABLE Page
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+createdAt timestamp NULL DEFAULT NULL COMMENT 'When was created',
+updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
 title VARCHAR(64) NOT NULL DEFAULT 'page header' COMMENT 'Page header',
 description VARCHAR(128) NULL COMMENT 'Small description after header',
 alias VARCHAR(128) NOT NULL COMMENT 'Link at this page'
@@ -20,6 +24,8 @@ alias VARCHAR(128) NOT NULL COMMENT 'Link at this page'
 CREATE TABLE News
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+createdAt timestamp NULL DEFAULT NULL COMMENT 'When was created',
+updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
 title VARCHAR(64) NOT NULL DEFAULT 'news header' COMMENT 'News header',
 description VARCHAR(128) NULL COMMENT 'Small description after header',
 content TEXT NOT NULL COMMENT 'News content',
@@ -31,6 +37,8 @@ image_id INT(11) NULL DEFAULT NULL COMMENT 'Image id from Images'
 CREATE TABLE Gallery
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+createdAt timestamp NULL DEFAULT NULL COMMENT 'When was created',
+updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
 title VARCHAR(64) NOT NULL DEFAULT 'gallery name' COMMENT 'Gallery name',
 alias VARCHAR(128) NOT NULL COMMENT 'Link at this page',
 status VARCHAR(32) NOT NULL DEFAULT 'draft' COMMENT 'Status. For example: publish/draft',
@@ -40,6 +48,8 @@ image_id INT(11) NULL DEFAULT NULL COMMENT 'Image id from Images'
 CREATE TABLE Post
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+createdAt timestamp NULL DEFAULT NULL COMMENT 'When was created',
+updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
 content TEXT NOT NULL COMMENT 'Post content',
 alias VARCHAR(128) NOT NULL COMMENT 'Link at this page',
 status VARCHAR(32) NOT NULL DEFAULT 'draft' COMMENT 'Status. For example: publish/draft',
@@ -49,6 +59,8 @@ image_id INT(11) NULL DEFAULT NULL COMMENT 'Image id from Images'
 CREATE TABLE Product
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+createdAt timestamp NULL DEFAULT NULL COMMENT 'When was created',
+updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
 title VARCHAR(64) NOT NULL DEFAULT 'product name' COMMENT 'Product name',
 description VARCHAR(128) NULL COMMENT 'Small description after header',
 content TEXT NOT NULL COMMENT 'About product',

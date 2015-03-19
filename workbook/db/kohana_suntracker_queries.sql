b9bdf09c491e39481355dbf5921c49eff884d727
Anton Lunenko <lunenko@dilegences.com>
2015-03-19 14:11:04 +0200

    from lowercase to uppercase TIMESTAMP; recheck require and default fields

--- workbook/db/kohana_suntracker_queries.sql
+++ workbook/db/kohana_suntracker_queries.sql
@@ -5,68 +5,66 @@ USE kohana;
 CREATE TABLE Image
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(64) NOT NULL DEFAULT 'image title' COMMENT 'Image title',
-name VARCHAR(128) NOT NULL COMMENT 'File name',
-created_at timestamp NULL DEFAULT NULL COMMENT 'When was created',
-updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
+title VARCHAR(64) NULL DEFAULT NULL COMMENT 'Image title',
+file_name VARCHAR(128) NOT NULL COMMENT 'File name',
+created_at TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
+updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
 ) CHARASET='utf8' COLLATE='utf8_general_ci' ENGINE=InnoDB COMMENT 'Single images';
 
 CREATE TABLE Page
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(64) NOT NULL DEFAULT 'page header' COMMENT 'Page header',
-description VARCHAR(128) NULL COMMENT 'Small description after header',
+title VARCHAR(64) NOT NULL COMMENT 'Page header',
+description VARCHAR(128) NULL DEFAULT NULL COMMENT 'Small description after header',
 alias VARCHAR(128) NOT NULL COMMENT 'Link at this page',
-created_at timestamp NULL DEFAULT NULL COMMENT 'When was created',
-updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
+created_at TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
+updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
 ) CHARASET='utf8' COLLATE='utf8_general_ci' ENGINE=InnoDB COMMENT 'Type of page with header and small description';
 
 CREATE TABLE News
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 image_id INT(11) NULL DEFAULT NULL COMMENT 'Image id from Images',
-title VARCHAR(64) NOT NULL DEFAULT 'news header' COMMENT 'News header',
-description VARCHAR(128) NULL COMMENT 'Small description after header',
+title VARCHAR(64) NOT NULL COMMENT 'News header',
+description VARCHAR(128) NULL DEFAULT NULL COMMENT 'Small description after header',
 content TEXT NOT NULL COMMENT 'News content',
 alias VARCHAR(128) NOT NULL COMMENT 'Link at this page',
 status TINYINT(3) NOT NULL DEFAULT '1' COMMENT 'Status. See Enum_Status',
-created_at timestamp NULL DEFAULT NULL COMMENT 'When was created',
-updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
+created_at TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
+updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
 ) CHARASET='utf8' COLLATE='utf8_general_ci' ENGINE=InnoDB COMMENT 'News content';
 
 CREATE TABLE Gallery
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-image_id INT(11) NULL DEFAULT NULL COMMENT 'Image id from Images',
-title VARCHAR(64) NOT NULL DEFAULT 'gallery name' COMMENT 'Gallery name',
+image_id INT(11) NOT NULL COMMENT 'Image id from Images',
+title VARCHAR(64) NOT NULL COMMENT 'Gallery name',
 alias VARCHAR(128) NOT NULL COMMENT 'Link at this page',
 status TINYINT(3) NOT NULL DEFAULT '1' COMMENT 'Status. See Enum_Status',
-created_at timestamp NULL DEFAULT NULL COMMENT 'When was created',
-updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
+created_at TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
+updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
 ) CHARASET='utf8' COLLATE='utf8_general_ci' ENGINE=InnoDB COMMENT 'Images collected in proups';
 
 CREATE TABLE Post
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 image_id INT(11) NULL DEFAULT NULL COMMENT 'Image id from Images',
-createdAt timestamp NULL DEFAULT NULL COMMENT 'When was created',
-updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed',
 content TEXT NOT NULL COMMENT 'Post content',
 alias VARCHAR(128) NOT NULL COMMENT 'Link at this page',
 status TINYINT(3) NOT NULL DEFAULT '1' COMMENT 'Status. See Enum_Status',
-created_at timestamp NULL DEFAULT NULL COMMENT 'When was created',
-updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
+created_at TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
+updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
 ) CHARASET='utf8' COLLATE='utf8_general_ci' ENGINE=InnoDB COMMENT 'Post';
 
 CREATE TABLE Product
 (
 id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 image_id INT(11) NULL DEFAULT NULL COMMENT 'Image id from Images',
-title VARCHAR(64) NOT NULL DEFAULT 'product name' COMMENT 'Product name',
-description VARCHAR(128) NULL COMMENT 'Small description after header',
+title VARCHAR(64) NOT NULL COMMENT 'Product name',
+description VARCHAR(128) NULL DEFAULT NULL COMMENT 'Small description after header',
 content TEXT NOT NULL COMMENT 'About product',
 alias VARCHAR(128) NOT NULL COMMENT 'Link at this page',
 status TINYINT(3) NOT NULL DEFAULT '1' COMMENT 'Status. See Enum_Status',
-created_at timestamp NULL DEFAULT NULL COMMENT 'When was created',
-updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
+created_at TIMESTAMP NULL DEFAULT NULL COMMENT 'When was created',
+updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'When was changed'
 ) CHARASET='utf8' COLLATE='utf8_general_ci' ENGINE=InnoDB COMMENT 'Product';
